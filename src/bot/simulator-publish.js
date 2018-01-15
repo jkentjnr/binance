35208b8d8a541a164b30bdf88e88dfdc5d85399f
James Kent <james.kent@vertic.com.au>
2018-01-15 20:09:55 +1100

    Better sample

--- src/bot/simulator-publish.js
+++ src/bot/simulator-publish.js
@@ -12,17 +12,17 @@ const fromDate = new Date(2018, 0, 14, 9, 0, 0, 0); // '20180114T090000'
 const toDate = new Date(2018, 0, 15, 13, 0, 0, 0);  // '20180115T130000'
 
 const buy_firstOffsetList = Array.from(Array(5).keys()).map(i => 12 + (i * 4));
-const buy_firstRequiredIncreaseList = Array.from(Array(5).keys()).map(i => 1 + .005 + (i * 0.006));
+const buy_firstRequiredIncreaseList = Array.from(Array(5).keys()).map(i => 1 + .002 + (i * 0.003));
 const buy_secondOffsetList = Array.from(Array(4).keys()).map(i => 16 + (i * 8));
-const buy_secondRequiredIncreaseList = Array.from(Array(5).keys()).map(i => 1 + .01 + (i * 0.012));
+const buy_secondRequiredIncreaseList = Array.from(Array(5).keys()).map(i => 1 + .002 + (i * 0.0035));
 
-const sell_protectDecreaseList = Array.from(Array(4).keys()).map(i => 0.995 - (i * 0.0025));
-const sell_firstOffsetList = Array.from(Array(5).keys()).map(i => 8 + (i * 4));
+const sell_protectDecreaseList = Array.from(Array(4).keys()).map(i => 1 - 0.002 - (i * 0.003));
+const sell_firstOffsetList = Array.from(Array(5).keys()).map(i => 12 + (i * 4));
 const sell_firstRequiredDecreaseList = Array.from(Array(5).keys()).map(i => 1 - .005 - (i * 0.008));
-const sell_secondOffsetList = Array.from(Array(4).keys()).map(i => 16 + (i * 8));
+const sell_secondOffsetList = Array.from(Array(5).keys()).map(i => 16 + (i * 4));
 const sell_secondRequiredDecreaseList = Array.from(Array(5).keys()).map(i => 1 - .01 - (i * 0.012));
 const sell_timedProtectOffsetList = Array.from(Array(3).keys()).map(i => 20 + (i * 12));
-const sell_timedProtectDecreaseList = Array.from(Array(4).keys()).map(i => 0.995 - (i * 0.0025));
+const sell_timedProtectDecreaseList = Array.from(Array(4).keys()).map(i => 1 - 0.002 - (i * 0.003));
 
 /*
 const buy_firstOffsetList = Array.from(Array(8).keys()).map(i => 16 + (i * 2));
